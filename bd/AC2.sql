1296385f5985ec2d1a78309b0b7ce143256b1eb6
Lucas Camargo <camargolucasd@gmail.com>
2022-11-09 21:23:40 -0300

    REFACTOR - Correção de nomes e implementação

--- bd/AC2.sql
+++ bd/AC2.sql
@@ -3,18 +3,19 @@ create database Ac2;
 use Ac2;
 
 create table Usuario (
+  id int primary key auto_increment,
   login_usuario varchar(40) unique,
-  senha_usuario varchar(30),
+  senha_usuario varchar(30) not null,
   nome_usuario varchar(100) not null
   );
   
   create table Prestador_de_Serviço (
+  id int primary key auto_increment,
   nome varchar(50) not null,
   valor_hora decimal(5,2),
   nome_empresa varchar(100),
-  tempo_contrato char(20),
-  data_inicio char(20),
-  data_fim char(20),
+  data_inicio varchar(10),
+  data_fim varchar(10),
   descricao_serviço varchar(500)
   );
   
