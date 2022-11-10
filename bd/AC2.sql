781fd509e841855d92f97c64541aec8a2d42455a
Lucas Camargo <camargolucasd@gmail.com>
2022-11-09 21:31:44 -0300

    REFACTOR - Correção

--- bd/AC2.sql
+++ bd/AC2.sql
@@ -4,7 +4,7 @@ use Ac2;
 
 create table Usuario (
   id int primary key auto_increment,
-  login_usuario varchar(40) unique,
+  login_usuario varchar(40) unique not null,
   senha_usuario varchar(30) not null,
   nome_usuario varchar(100) not null
   );
@@ -12,7 +12,7 @@ create table Usuario (
   create table Prestador_de_Serviço (
   id int primary key auto_increment,
   nome varchar(50) not null,
-  valor_hora decimal(5,2),
+  valor_hora float,
   nome_empresa varchar(100),
   data_inicio varchar(10),
   data_fim varchar(10),
