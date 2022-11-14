e8e9b614ec53922e3cf9641f97f0cb0968822dc2
Felipe Sanches Franqueira <felipe04sfr@outlook.com>
2022-11-14 16:46:30 -0300

    FEAT - ServicoController

--- bd/AC2.sql
+++ bd/AC2.sql
@@ -9,13 +9,13 @@ create table Usuario (
   nome_usuario varchar(100) not null
   );
   
-  create table Prestador_de_Serviço (
+  create table Prestador_de_Servico (
   id int primary key auto_increment,
   nome varchar(50) not null,
   valor_hora float,
   nome_empresa varchar(100),
   data_inicio varchar(10),
   data_fim varchar(10),
-  descricao_serviço varchar(500)
+  descricao_servico varchar(500)
   );
   
